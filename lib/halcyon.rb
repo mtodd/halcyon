ed4272c894a4114b6d7ef0de11860e304fd96743
Matt Todd <chiology@gmail.com>
2008-03-03 17:23:50 -0500

    Adding the Application class to Halcyon and updating the core Halcyon module.

--- lib/halcyon.rb
+++ lib/halcyon.rb
@@ -6,42 +6,16 @@
 
 $:.unshift File.dirname(__FILE__)
 
-#--
-# dependencies
-#++
-
 %w(rubygems merb/core_ext).each {|dep|require dep}
 
-#--
-# module
-#++
-
 module Halcyon
-  VERSION = [0,4,0]
+  
+  VERSION = [0,5,0]
   def self.version
     VERSION.join('.')
   end
   
-  # = Introduction
-  # 
-  # Halcyon is a JSON Web Server Framework intended to be used for fast, small
-  # data transactions, like for AJAX-intensive sites or for special services like
-  # authentication centralized for numerous web apps in the same cluster.
-  # 
-  # The possibilities are pretty limitless: the goal of Halcyon was simply to be
-  # lightweight, fast, simple to implement and use, and able to be extended.
-  # 
-  # == Usage
-  # 
-  # For documentation on using Halcyon, check out the Halcyon::Server::Base and
-  # Halcyon::Client::Base classes which contain much more usage documentation.
-  def introduction
-    abort "READ THE DAMNED RDOCS!"
-  end
-  
-  #--
-  # Module Autoloading
-  #++
+  autoload :Application, 'halcyon/application'
   
   class Server
     module Auth
