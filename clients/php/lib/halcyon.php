d4a29a572423e6eece8c7c8fc15833cd14d2f7f8
Matt Todd <chiology@gmail.com>
2008-03-14 17:01:20 -0400

    Added initial PHP Halcyon client library file and structure.

---
+++ clients/php/lib/halcyon.php
@@ -0,0 +1,18 @@
+<?php
+
+// Halcyon PHP Client
+// Created by Matt Todd <chiology@gmail.com>
+// Copyright (c) 2008, Matt Todd <http://purl.org/net/maraby>
+// MIT License applies to this library
+
+class Halcyon {
+  
+  const VERSION = "0.1.0";
+  
+  function __construct($uri, $app) {
+    //
+  }
+  
+}
+
+?>
