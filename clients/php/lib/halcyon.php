70e8c2bda07a0dcdbe7fb109be9c57df61eb8fc0
Matt Todd <chiology@gmail.com>
2008-03-21 12:42:10 -0400

    Added support for custom headers for requests in the PHP client. Also refactored much of the way headers are handled in general and defaulted to.

--- clients/php/lib/halcyon.php
+++ clients/php/lib/halcyon.php
@@ -10,49 +10,64 @@ class Halcyon {
   const VERSION = "0.1.0";
   
   private $uri = array('host' => 'localhost', 'port' => 4647, 'scheme' => 'http');
+  private $headers = array(
+    'Content-Type' => 'application/json',
+    'User-Agent' => 'JSON/1.2.1 Compatible (en-US) Halcyon::Client/0.5.0',
+    'Connection' => 'close'
+  );
   
-  public function __construct($uri = null) {
+  public function __construct($uri = null, $headers = null) {
     if($uri) $this->uri = parse_url($uri);
+    if(isset($headers)) {
+      foreach($headers as $key => $value) {
+        $this->header($key, $value);
+      }
+    }
   }
   
-  protected function get($path) {
+  public function header($key, $value) {
+    $this->headers[$key] = $value;
+  }
+  
+  protected function get($path, $headers = array()) {
     return $this->request(array(
       'method' => 'GET',
       'path' => $path
-    ));
+    ), $headers);
   }
   
-  protected function post($path, $data) {
+  protected function post($path, $data, $headers = array()) {
     return $this->request(array(
       'method' => 'POST',
       'path' => $path,
       'body' => $data
-    ));
+    ), $headers);
   }
   
-  protected function delete($path) {
+  protected function delete($path, $headers = array()) {
     return $this->request(array(
       'method' => 'DELETE',
       'path' => $path
-    ));
+    ), $headers);
   }
   
-  protected function put($path, $data) {
+  protected function put($path, $data, $headers = array()) {
     return $this->request(array(
       'method' => 'PUT',
       'path' => $path,
       'body' => $data
-    ));
+    ), $headers);
   }
   
-  private function request($request) {
+  private function request($request, $headers = array()) {
     $connection = fsockopen($this->uri['host'], $this->uri['port']);
     if($connection) {
       // connected
       $req = "{$request['method']} {$request['path']} HTTP/1.1\r\n";
-      $req .= "Content-Type: application/json\r\n";
-      $req .= "User-Agent: JSON/1.2.1 Compatible (en-US) Halcyon::Client/0.5.0";
-      $req .= "Connection: close\r\n";
+      foreach(array_merge($this->headers, $headers) as $key => $value) {
+        if($value == null) continue;
+        $req .= "{$key}: {$value}\r\n";
+      }
       $req .= "\r\n";
       if($request['body']) $req .= http_build_query($request['body']) . "\r\n\r\n";
       
