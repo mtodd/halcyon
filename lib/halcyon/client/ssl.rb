aa8e9b8e101d193c897f5c696ec616b02c74325d
Matt Todd <chiology@gmail.com>
2008-05-16 15:10:35 -0400

    Updated the SSL-variant of the Client to match the changes to the headers.

--- lib/halcyon/client/ssl.rb
+++ lib/halcyon/client/ssl.rb
@@ -10,23 +10,20 @@ module Halcyon
       req["User-Agent"] = USER_AGENT
       
       # apply provided headers
-      headers.each do |(header, value)|
+      self.headers.merge(headers).each do |(header, value)|
         req[header] = value
       end
       
-      # provide hook for modifying the headers
-      req = headers(req) if respond_to? :headers
-      
       # prepare and send HTTPS request
-      serv = Net::HTTP.new(@uri.host, @uri.port)
-      serv.use_ssl = true if @uri.scheme == 'https'
+      serv = Net::HTTP.new(self.uri.host, self.uri.port)
+      serv.use_ssl = true if self.uri.scheme == 'https'
       res = serv.start {|http|http.request(req)}
       
       # parse response
       body = JSON.parse(res.body).to_mash
       
       # handle non-successes
-      if @options[:raise_exceptions] && !res.kind_of?(Net::HTTPSuccess)
+      if self.options[:raise_exceptions] && !res.kind_of?(Net::HTTPSuccess)
         raise self.class.const_get(Exceptions::HTTP_STATUS_CODES[body[:status]].tr(' ', '_').camel_case.gsub(/( |\-)/,'')).new
       end
       
